aadb189bc7e10c61254f7f806962fe4836ae43d9
Shane <shanefengcn@hotmail.com>
2018-06-04 13:12:59 +0800

    Upload ZhuiXinFan

---
+++ ZhuiXinFan.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+
+# Thanks: CodesW(https://www.cnblogs.com/tp1226/p/8419564.html)
+
+import requests
+from bs4 import BeautifulSoup
+
+domain = 'http://www.zhuixinfan.com/'
+emule = []
+baidu = []
+weiyun = []
+
+tv_url = input('请输入剧集地址\n格式为: http://www.zhuixinfan.com/viewtvplay-[数字].html\n')
+
+with requests.Session() as session:
+    resp = session.get(tv_url, timeout=10)
+    resp.raise_for_status()
+    soup = BeautifulSoup(resp.content, 'lxml', from_encoding=resp.encoding)
+    tag_resources = soup.find(id='ajax_tbody')
+    for res in tag_resources.find_all('tr'):
+        tag_a = res.find('a')
+
+        res_url = domain + tag_a.get('href')
+        resp = session.get(res_url, timeout=10)
+        resp.raise_for_status()
+        soup = BeautifulSoup(resp.content, 'lxml', from_encoding=resp.encoding)
+
+        emule_url = soup.find(id='emule_url')
+        emule.append(emule_url.get_text())
+
+        baidu_text = soup.find(text='网盘下载1')
+        baidu_parent = baidu_text.parent
+        baidu.append(baidu_parent.get('href'))
+
+        weiyun_text = soup.find(text='网盘下载2')
+        weiyun_parent = weiyun_text.parent
+        weiyun.append(weiyun_parent.get('href'))
+
+print('ed2k:')
+for i in range(len(emule)):
+    print(emule[i])
+
+print('百度云:')
+for j in range(len(baidu)):
+    print(baidu[j])
+
+print('微云:')
+for k in range(len(weiyun)):
+    print(weiyun[k])
